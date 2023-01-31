839521de56e8601c1e5fc3b8bbff4e75c98fe828
Adin Schmahmann <adin.schmahmann@gmail.com>
2023-01-31 16:24:39 -0500

    chore: stub out types to libipfs

--- util.go
+++ util.go
@@ -1,25 +1,8 @@
 package files
 
-// ToFile is an alias for n.(File). If the file isn't a regular file, nil value
-// will be returned
-func ToFile(n Node) File {
-	f, _ := n.(File)
-	return f
-}
+import "github.com/ipfs/go-libipfs/files"
 
-// ToDir is an alias for n.(Directory). If the file isn't directory, a nil value
-// will be returned
-func ToDir(n Node) Directory {
-	d, _ := n.(Directory)
-	return d
-}
-
-// FileFromEntry calls ToFile on Node in the given entry
-func FileFromEntry(e DirEntry) File {
-	return ToFile(e.Node())
-}
-
-// DirFromEntry calls ToDir on Node in the given entry
-func DirFromEntry(e DirEntry) Directory {
-	return ToDir(e.Node())
-}
+var ToFile = files.ToFile
+var ToDir = files.ToDir
+var FileFromEntry = files.FileFromEntry
+var DirFromEntry = files.DirFromEntry
